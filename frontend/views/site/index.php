a7e98d013a23308fd41fc5b713c2dff1e906d159
muhammad aufa <am.rifqi@ymail.com>
2017-11-10 09:16:31 +0700

    Test

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -5,7 +5,7 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Assalamualikum !!</h1>
+        <h1>Assalamualikum Akhi!!</h1>
 
         <p class="lead">You have successfully created your Yii-powered application.</p>
 
